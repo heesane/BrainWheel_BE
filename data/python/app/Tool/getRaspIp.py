db7de1ba4f74bf0466d7a4e11ce34c4485f7a535
heesane <tees3359@gmail.com>
2023-05-01 00:05:36 +0900

    Change Lib

--- data/python/app/Tool/getRaspIp.py
+++ data/python/app/Tool/getRaspIp.py
@@ -1,4 +1,4 @@
-# SIDE : Raspberry Pi
+# SIDE : Raspberz
 import socket
 
 def get_local_ip_address():
@@ -7,12 +7,8 @@ def get_local_ip_address():
         s.connect(("8.8.8.8", 80))
         local_ip = s.getsockname()[0]
         s.close()
+        
     except socket.error:
         local_ip = None
+        
     return local_ip
-
-rasp_ip = get_local_ip_address()
-if rasp_ip is None:
-    print("라즈베리파이 IP 주소를 가져올 수 없습니다.")
-else:
-    print("라즈베리파이 IP 주소:", rasp_ip)
